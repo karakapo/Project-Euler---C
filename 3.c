fe4a56bbb96f2bb2c32a67a5a5ef65e3b8d59ce9
karakapo <morgonkaraa@gmail.com>
2024-09-04 13:19:00 +0300

    3.c

---
+++ 3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int checkprime(unsigned long long int num);
+main() {
+	int counter;
+	unsigned long long int  i;
+	unsigned long long int num = 600851475143; 
+	 for(i =2;i<=num;i++){ 
+            counter = 0; 
+            while(checkprime(i) == 1 && (num%i) == 0){ 
+                num = num/i; 
+                counter++; 
+            }  
+            if(counter>0){
+                printf("\n%llu içinde %d kez var",i,counter);
+            }	
+}
+}
+int checkprime(unsigned long long int num) { 
+	int b,prime=1;
+	for(b=2;b<=num/2;b++) {
+		if( (num%b)==0) {
+		prime=0;
+		break;
+		}
+	}
+	return prime;
+}
